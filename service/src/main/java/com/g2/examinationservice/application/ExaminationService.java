156d17909606e3f9277540a0c3ff83febbd25919
Simon Blomsterlund <simon@simonblund.com>
2020-11-16 21:50:10 +0200

    Begin work on examination-service.

--- service/src/main/java/com/g2/examinationservice/application/ExaminationService.java
+++ service/src/main/java/com/g2/examinationservice/application/ExaminationService.java
@@ -1,4 +1,9 @@
 package com.g2.examinationservice.application;
 
+import com.g2.examinationservice.domain.Examination;
+
 public interface ExaminationService {
+    Examination save(Examination exam);
+    Examination getExamination(String examinationCode);
+    Examination getExaminationsFromCourse(String courseCode);
 }
--- service/src/main/java/com/g2/examinationservice/application/impl/ExaminationServiceImpl.java
+++ service/src/main/java/com/g2/examinationservice/application/impl/ExaminationServiceImpl.java
@@ -1,6 +1,21 @@
 package com.g2.examinationservice.application.impl;
 
 import com.g2.examinationservice.application.ExaminationService;
+import com.g2.examinationservice.domain.Examination;
 
 public class ExaminationServiceImpl implements ExaminationService {
+    @Override
+    public Examination save(Examination exam) {
+        return null;
+    }
+
+    @Override
+    public Examination getExamination(String examinationCode) {
+        return null;
+    }
+
+    @Override
+    public Examination getExaminationsFromCourse(String courseCode) {
+        return null;
+    }
 }
